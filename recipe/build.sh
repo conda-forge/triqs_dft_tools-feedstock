1aed0a997ffe484e2eb61855491255f174c93df8
Thomas Hahn <thomas.hahn01@gmail.com>
2024-11-19 19:06:32 -0500

    Try to fix python detection issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,15 +9,15 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "1" ]]; then
   export OPAL_PREFIX="$PREFIX"
 
   # Hack for python > 3.11
-  py_version=$( python -c "import sys; print('{}.{}'.format(sys.version_info[0], sys.version_info[1]))" )
-  if [[ "$py_version" > "3.11" ]]; then
+  py_minor=$( python -c "import sys; print('{}'.format(sys.version_info[1]))" )
+  if [[ "$py_minor" -gt "11" ]]; then
     # Change the meson_cross_file.txt (see https://github.com/conda-forge/numpy-feedstock/blob/main/recipe/build.sh)
     echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
     echo "[properties]" >> ${CONDA_PREFIX}/meson_cross_file.txt
     echo "longdouble_format = 'IEEE_DOUBLE_LE'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
     # Change the f2py module to use meson for cross compilation
-    f2py_file=${BUILD_PREFIX}/lib/python3.12/site-packages/numpy/f2py/_backends/_meson.py
+    f2py_file=${BUILD_PREFIX}/lib/python3.${py_minor}/site-packages/numpy/f2py/_backends/_meson.py
     sed "s|\"setup\",|\"setup\"] + [x for x in \"${MESON_ARGS}\".split()] + [|" ${f2py_file} > tmp_file
     cp tmp_file ${f2py_file}
   fi
